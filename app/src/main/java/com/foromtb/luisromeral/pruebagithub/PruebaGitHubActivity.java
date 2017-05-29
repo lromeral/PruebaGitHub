d42fd5a348591622e51d60082cf1a0a3dd8c5248
lromeral <luis.romeral@gmail.com>
2017-05-29 15:05:23 +0200

    Initial commit

---
+++ app/src/main/java/com/foromtb/luisromeral/pruebagithub/PruebaGitHubActivity.java
@@ -0,0 +1,13 @@
+package com.foromtb.luisromeral.pruebagithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PruebaGitHubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_git_hub);
+    }
+}
